07a8c36bbaef45ce9bac28e80d864a7ebe1f7a6f
overbye <overbye87@gmail.com>
2022-02-17 11:46:14 +0300

    add sequelize cli

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const sequelize = require("./models");
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
+const start = async () => {
+  try {
+    //sequelize.authenticate();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
