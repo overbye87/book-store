5bc1bb7ec2a99a5a9ac6a37e38d53aef1402590f
overbye <overbye87@gmail.com>
2022-02-18 14:57:08 +0300

    add bookreducer

---
+++ client/src/store/redusers/bookReducer.ts
@@ -0,0 +1,23 @@
+import { BookActionTypes, IBookAction, IBookState } from "../../types/books";
+
+const initialState: IBookState = {
+  books: [],
+  loading: false,
+  error: null,
+};
+
+export const bookReducer = (
+  state = initialState,
+  action: IBookAction
+): IBookState => {
+  switch (action.type) {
+    case BookActionTypes.FETCH_BOOKS:
+      return { loading: true, error: null, books: [] };
+    case BookActionTypes.FETCH_BOOKS_SUCCESS:
+      return { loading: false, error: null, books: action.payload };
+    case BookActionTypes.FETCH_BOOKS_ERROR:
+      return { loading: false, error: action.payload, books: [] };
+    default:
+      return state;
+  }
+};
---
+++ client/src/types/books.ts
@@ -0,0 +1,30 @@
+export enum BookActionTypes {
+  FETCH_BOOKS = "FETCH_BOOKS",
+  FETCH_BOOKS_SUCCESS = "FETCH_BOOKS_SUCCESS",
+  FETCH_BOOKS_ERROR = "FETCH_BOOKS_ERROR",
+}
+
+interface IFetchBooksAction {
+  type: BookActionTypes.FETCH_BOOKS;
+}
+
+interface IFetchBooksSuccessAction {
+  type: BookActionTypes.FETCH_BOOKS_SUCCESS;
+  payload: any[];
+}
+
+interface IFetchBooksErrorAction {
+  type: BookActionTypes.FETCH_BOOKS_ERROR;
+  payload: string | object;
+}
+
+export type IBookAction =
+  | IFetchBooksAction
+  | IFetchBooksSuccessAction
+  | IFetchBooksErrorAction;
+
+export interface IBookState {
+  books: any[];
+  loading: boolean;
+  error: null | string | object;
+}
