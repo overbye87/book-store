2db5e8343f59c051eb58988debac8cbcf53ad3ac
overbye <overbye87@gmail.com>
2022-03-13 19:44:58 +0300

    add redux-devtools

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,5 +1,18 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import { rootReducer } from "./redusers";
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers =
+  typeof window === "object" &&
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension's options like name, actionsDenylist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk)
+  // other store enhancers if any
+);
+
+export const store = createStore(rootReducer, enhancer);
